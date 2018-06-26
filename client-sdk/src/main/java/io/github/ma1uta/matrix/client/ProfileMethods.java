902b6e6e7e22def51618f77f41fdf307c9709f68
Anatoliy Sablin <sablintolya@gmail.com>
2018-06-26 23:02:49 +0300

    Added the profile methods.

--- client-sdk/src/main/java/io/github/ma1uta/matrix/client/ProfileMethods.java
+++ client-sdk/src/main/java/io/github/ma1uta/matrix/client/ProfileMethods.java
@@ -18,7 +18,9 @@ package io.github.ma1uta.matrix.client;
 
 import io.github.ma1uta.matrix.EmptyResponse;
 import io.github.ma1uta.matrix.client.api.ProfileApi;
+import io.github.ma1uta.matrix.client.model.profile.AvatarUrl;
 import io.github.ma1uta.matrix.client.model.profile.DisplayName;
+import io.github.ma1uta.matrix.client.model.profile.Profile;
 
 /**
  * Profile methods.
@@ -46,5 +48,55 @@ public class ProfileMethods {
         DisplayName request = new DisplayName();
         request.setDisplayname(displayName);
         requestMethods.put(ProfileApi.class, "setDisplayName", params, request, EmptyResponse.class);
+    }
+
+    /**
+     * Get the user's display name. This API may be used to fetch the user's own displayname or to query the name of other users; either
+     * locally or on remote homeservers.
+     *
+     * @param userId The user whose display name to get.
+     * @return The display name for this user.
+     */
+    public String showDisplayName(String userId) {
+        RequestParams params = new RequestParams().pathParam("userId", userId);
+        return getMatrixClient().getRequestMethods().get(ProfileApi.class, "showDisplayName", params, DisplayName.class).getDisplayname();
+    }
+
+    /**
+     * This API sets the given user's avatar URL. You must have permission to set this user's avatar URL, e.g. you need to have
+     * their access_token.
+     *
+     * @param avatarUrl The user whose avatar URL to set.
+     */
+    public void setAvaterUrl(String avatarUrl) {
+        String userId = getMatrixClient().getUserId();
+        RequestParams params = new RequestParams().pathParam("userId", userId);
+        AvatarUrl request = new AvatarUrl();
+        request.setAvatarUrl(avatarUrl);
+        getMatrixClient().getRequestMethods().put(ProfileApi.class, "setAvatarUrl", params, request, EmptyResponse.class);
+    }
+
+    /**
+     * Get the user's avatar URL. This API may be used to fetch the user's own avatar URL or to query the URL of other users;
+     * either locally or on remote homeservers.
+     *
+     * @param userId The user whose avatar URL to get.
+     * @return The avatar URL for this user.
+     */
+    public String showAvatarUrl(String userId) {
+        RequestParams params = new RequestParams().pathParam("userId", userId);
+        return getMatrixClient().getRequestMethods().get(ProfileApi.class, "showAvatarUrl", params, AvatarUrl.class).getAvatarUrl();
+    }
+
+    /**
+     * Get the combined profile information for this user. This API may be used to fetch the user's own profile information or
+     * other users; either locally or on remote homeservers. This API may return keys which are not limited to displayname or avatar_url.
+     *
+     * @param userId The user whose profile information to get.
+     * @return The profile info for this user.
+     */
+    public Profile profile(String userId) {
+        RequestParams params = new RequestParams().pathParam("userId", userId);
+        return getMatrixClient().getRequestMethods().get(ProfileApi.class, "profile", params, Profile.class);
     }
 }
