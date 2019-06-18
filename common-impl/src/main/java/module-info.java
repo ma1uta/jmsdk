db483ce4b3a40898ea0b1068115c0b3ed64819f2
Anatoly Sablin <sablintolya@gmail.com>
2019-06-18 22:40:15 +0300

    Downgrade to java 8. Migrate to microprofile.

--- common-impl/src/main/java/module-info.java
+++ common-impl/src/main/java/module-info.java
@@ -2,6 +2,6 @@ module matrix.common.impl {
     exports io.github.ma1uta.matrix.impl.exception;
 
     requires transitive matrix.common.api;
-    requires transitive org.slf4j;
+    requires transitive slf4j.api;
     requires transitive java.ws.rs;
 }
