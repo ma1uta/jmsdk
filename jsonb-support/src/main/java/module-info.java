122a47c8709ceaa2e18b1c8d12e4304dfc686926
Anatoly Sablin <sablintolya@gmail.com>
2019-06-23 00:26:34 +0300

    Fix java-module.

--- jsonb-support/src/main/java/module-info.java
+++ jsonb-support/src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module matrix.support.jsonb {
     exports io.github.ma1uta.matrix.support.jsonb;
+    exports io.github.ma1uta.matrix.support.jsonb.mapper to org.mapstruct;
 
     requires transitive matrix.common.api;
     requires transitive java.json;
+    requires org.mapstruct;
+    requires java.sql;
 }
