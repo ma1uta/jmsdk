bb8dfab85daf830bd648d8ea5513cdae5b4ea447
Anatoly Sablin <sablintolya@gmail.com>
2019-06-22 16:01:04 +0300

    Use the latest stable version.

--- client-jaxrs/src/main/java/module-info.java
+++ client-jaxrs/src/main/java/module-info.java
@@ -1,5 +1,6 @@
 module matrix.client.jaxrs {
     exports io.github.ma1uta.matrix.client.factory.jaxrs;
+    opens io.github.ma1uta.matrix.client.factory.jaxrs;
 
     requires transitive matrix.client.impl;
     requires transitive matrix.support.jackson;
