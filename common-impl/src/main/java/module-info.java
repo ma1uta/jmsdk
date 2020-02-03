397d6ef2e9de54ec03f3969439384a5b128fe487
Anatoly Sablin <sablintolya@gmail.com>
2020-02-03 22:33:16 +0300

    Update dependencies. Update checkstyle configuration.

--- common-impl/src/main/java/module-info.java
+++ common-impl/src/main/java/module-info.java
@@ -3,7 +3,7 @@ module matrix.common.impl {
     exports io.github.ma1uta.matrix.impl.exception;
 
     requires transitive matrix.common.api;
-    requires transitive slf4j.api;
+    requires transitive org.slf4j;
     requires transitive java.ws.rs;
     requires transitive microprofile.rest.client.api;
 }
